117f31317d1c6d6426213ddec6cc525bea693dcf
SeHoonYang <apaphpmys@kaist.ac.kr>
2017-05-07 18:19:14 +0900

    Find all does not find all
    해결좀 해주셈

--- articleCrawler.py
+++ articleCrawler.py
@@ -1,7 +1,10 @@
-from bs4 import BeautifulSoup as BS
+﻿from bs4 import BeautifulSoup as BS
 import urllib.request
 
 def get_text(URL):
     source_code_from_URL = urllib.request.urlopen(URL);
     soup = BS(source_code_from_URL, 'html.parser', from_encoding = 'utf-8');
-    return soup.find_all('div', id='dic_area')
+    print(soup.find_all('div', id="dic_area"))
+
+#print(get_text("http://m.news.naver.com/read.nhn?oid=079&aid=0002964148"))
+get_text("http://m.news.naver.com/read.nhn?oid=079&aid=0002964148")
